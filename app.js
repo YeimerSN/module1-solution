24b2a1858131996ba4e3f54c91d49b1888cddb46
YeimerSN <yeimer.serrano@gmail.com>
2021-06-11 17:57:30 -0500

    Module one completed.

--- app.js
+++ app.js
@@ -12,19 +12,26 @@
 
         function Lunch(string){
             var mesagge = "";
+            var empty = false;
             var elements = string.split(',');
-            console.log(elements)
-            if(elements.length == 1){
-                mesagge = "Please enter data first";
-            }else if(elements.length > 1 && elements.length < 4){
-                mesagge = "Enjoy!";
-            }else if(elements.length >= 4){
-                mesagge = "Too much!";
+            for(var i = 0; i <elements.length; i++){
+                if(elements.length != 1 && (elements[i] == "" || elements[i] == " ")){
+                    empty = true;
+                }
             }
-
+            if(empty == false){
+                if(elements == ""){
+                    mesagge = "Please enter data first";
+                }else if(elements.length > 0 && elements.length < 4){
+                    mesagge = "Enjoy!";
+                }else if(elements.length >= 4){
+                    mesagge = "Too much!";
+                }
+            }else{
+                mesagge = "Element empty! Check your input.";
+            }
             return mesagge;
         };
-
     });
 
 })();
